c3ff2a8c329d663d7cd7202c1ba69c686ff9a964
farhan5548 <farhanfareed450@gmail.com>
2025-10-07 12:37:56 +0500

    Practice for Button & TextField

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,55 @@
-import { Typography, Button } from '@mui/material'
+import { Typography, Button, TextField } from '@mui/material'
 import './App.css'
-
 function App() {
 
   return (
     <>
-      <Typography variant='h2' sx={{ color: "red" }}>Sample Text</Typography>
-      <div>
+      <Typography variant='h3'
+        sx={{
+          bgcolor: "red",
+          fontWeight: 'bold',
+          fontStyle: 'italic',
+          textDecoration: 'underline',
+          textAlign:'center',
+          mb:'2rem',
+          py:'2rem'
+        }}
+      >Ya to Win ha, Ya to Learn ha</Typography>
+      <div style={{marginLeft:'2rem', marginRight:'2rem'}}>
         <h1>MUI Tutorial</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, quaerat!</p>
-        <Button variant="contaned" sx={{color:"success"}}>Click Here</Button>
-        <Button variant="text">Click Here</Button>
-        <Button variant="outlined">Click Here</Button>
+        <Button
+          variant="contained"
+          color='success'
+          sx={{ m: '10px' }}
+          onClick={() => alert("Button is Clicked By User")}
+        >Click Here</Button>
+
+        <Button variant="text"
+          disabled
+        >Click Here</Button>
+
+        <Button variant="outlined"
+          href='https://github.com/farhan5548'
+          sx={{ px: '2rem', border: '2px solid black', bgcolor: 'yellow', color: 'black' }}
+        >GitHub Profile</Button>
+
+        <br />
+        {/* <input type="text" placeholder='Name' /> */}
+        <TextField type="text"
+          placeholder='Name'
+          variant='standard'
+          sx={{ m: '30px' }} />
+
+        <TextField type="text"
+          placeholder='Name'
+          variant='outlined'
+          sx={{ m: '30px' }} />
+
+        <TextField type="text"
+          placeholder='Name'
+          variant='filled'
+          sx={{ m: '30px' }} />
       </div>
     </>
   )
